96f7a875a0d576970fd8f31164ade58ee077fba1
sonal <sonalkarle01@gmail.com>
2021-03-10 18:06:40 +0530

    Create table in database

--- Emppayroll/EmpPayrollErdiagram.sql
+++ Emppayroll/EmpPayrollErdiagram.sql
@@ -2,3 +2,13 @@
 create database Emppayroll;
 SELECT * FROM sys.databases ; 
 use Emppayroll;
+
+--UC2:Create table for employee payroll
+create Table Emppayroll
+(
+ID			int	identity(1, 1),
+Name		varchar(255) NOT Null,
+Salary		int,
+StartDate	Date,
+primary key(ID)
+)
